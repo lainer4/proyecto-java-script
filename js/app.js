86a7312153727ab18141e443694e3b8231370893
jairo lainer moreno guevara <jmjlainer24@gmail.com>
2017-09-08 22:15:06 -0500

    operaciones
    operaciones

--- js/app.js
+++ js/app.js
@@ -21,6 +21,106 @@ var Calculadora = {
 		document.getElementById(tecla).style.transform="scale(0.9)";
 		setTimeout(function() {document.getElementById(tecla).style.transform="scale(1)";}, 200);
 	},
+	menos: function(){
+ 		 this.animacionuno("menos");
+ 	 this.num1 = Number(this.pantalla);
+ 	 this.pantalla = "",
+ 	 this.opcion = 2,
+ 	 this.auxestado = 0,
+ 	 this.signo = 0,
+ 	 this.auxnum = 0,
+ 	 this.auxestado = 0,
+ 	 this.decimal = 0,
+ 	 this.viewdisplay();
+ },
+ /*funcion multiplica*/
+  por: function(){
+ 		 this.animacionuno("por");
+ 	 this.num1 = Number(this.pantalla),
+ 	 this.pantalla = "",
+ 	 this.opcion = 3,
+ 	 this.auxestado = 0,
+ 	 this.signo = 0,
+ 	 this.auxnum = 0,
+ 	 this.auxestado = 0,
+ 	 this.decimal = 0,
+ 	 this.viewdisplay();
+ },
+ /*funcion dividir*/
+  dividido: function(){
+ 		 this.animacionuno("dividido");
+ 	 this.num1 = Number(this.pantalla),
+ 	 this.pantalla = "",
+ 	 this.opcion = 4,
+ 	 this.auxestado = 0,
+ 	 this.signo = 0,
+ 	 this.auxnum = 0,
+ 	 this.auxestado = 0,
+ 	 this.decimal = 0,
+ 	 this.viewdisplay();
+ },
+ /*funcion sumar*/
+ mas: function(){
+ 		 this.animacionuno("mas");
+ 	 this.num1 += Number(this.pantalla),
+ 	 this.pantalla = "",
+ 	 this.opcion = 1,
+ 	 this.auxestado = 0,
+ 	 this.signo = 0,
+ 	 this.auxnum = 0,
+ 	 this.auxestado = 0,
+ 	 this.decimal = 0,
+ 	 this.viewdisplay();
+ },
+ /*funciÃ³n igual*/
+ igual: function(){
+  this.animacionuno("igual");
+  switch(this.opcion){
+ 	 case 1:
+ 			 if(this.auxestado == 0){
+ 				 this.auxnum = Number(this.pantalla),
+ 				 this.pantalla = this.num1 + Number(this.pantalla),
+ 				 this.auxestado = 1,
+ 				 this.num1 = 0;
+ 			 }else{
+ 				 this.pantalla = Number(this.pantalla)+this.auxnum;
+ 			 }
+ 		 break;
+ 	 case 2:
+ 			 if(this.auxestado == 0){
+ 				 this.auxnum = Number(this.pantalla),
+ 				 this.pantalla = this.num1 - Number(this.pantalla),
+ 				 this.auxestado = 1,
+ 				 this.num1 = 0;
+ 			 }else{
+ 				 this.pantalla = Number(this.pantalla)-this.auxnum;
+ 			 }
+ 		 break;
+ 	 case 3:
+ 			 if(this.auxestado == 0){
+ 				 this.auxnum = Number(this.pantalla),
+ 				 this.pantalla = this.num1 * Number(this.pantalla),
+ 				 this.auxestado = 1,
+ 				 this.num1 = 0;
+ 			 }else{
+ 				 this.pantalla = Number(this.pantalla)*this.auxnum;
+ 			 }
+ 		 break;
+ 	 case 4:
+ 			 if(this.auxestado == 0){
+ 				 this.auxnum = Number(this.pantalla),
+ 				 this.pantalla = this.num1 / Number(this.pantalla),
+ 				 this.auxestado = 1,
+ 				 this.num1 = 0;
+ 			 }else{
+ 				 this.pantalla = Number(this.pantalla)/this.auxnum;
+ 			 }
+ 		 break;
+ 	 default:
+ 		 break;
+  }
+  this.viewdisplay();
+ },
 
 
 
@@ -44,7 +144,7 @@ var Calculadora = {
 		document.getElementById("igual").addEventListener("click",function(){Calculadora.igual()});
 		document.getElementById("mas").addEventListener("click",function(){Calculadora.mas()});
 		document.getElementById("por").addEventListener("click",function(){Calculadora.por()});
-    
+
 	},
 
 
